018748bc3de149bc128de4e1157152dbce6962c8
Milagro <93047873+UMlopez@users.noreply.github.com>
2021-11-07 22:43:20 -0500

    PagarCita

---
+++ ProyectoClinica/src/app/pages/pagarcitas/pagarcitas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pagarcitas',
+  templateUrl: './pagarcitas.component.html',
+  styleUrls: ['./pagarcitas.component.css']
+})
+export class PagarcitasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
