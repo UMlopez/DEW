04a07ffcfcd9bb0b0af4cfbb8da8438894f487ab
Milagro <93047873+UMlopez@users.noreply.github.com>
2021-11-27 16:22:08 -0500

    Validacion Captcha
    se modifico login.html y login.ts

--- ProyectoClinica/src/app/pages/login/login.component.ts
+++ ProyectoClinica/src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RecaptchaErrorParameters } from 'ng-recaptcha';
 
 @Component({
   selector: 'app-login',
@@ -7,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+   public resolved(captchaResponse : string) : void {
+     console.log ('Captcha Resuelto : ${captchaResponse}')
+   }
+   public onError(errorDetails: RecaptchaErrorParameters) : void {
+     console.log('Ocurrio un error : ${errordetails}')
+   }
   constructor() { }
 
   ngOnInit(): void {
